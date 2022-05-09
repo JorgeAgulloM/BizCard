48525495db9780f7a4aab87c614faf52724efe92
Jorge Agullo <agullojorge@gmail.com>
2022-05-09 12:13:59 +0200

    [FEATURE] WIP BackGround - profile user

--- app/src/main/java/com/practice/bizcard/MainActivity.kt
+++ app/src/main/java/com/practice/bizcard/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.practice.bizcard
 
+import android.media.ImageReader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.practice.bizcard.ui.theme.BizCardTheme
@@ -49,6 +58,26 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 4.dp
         ) {
+            Column(modifier = Modifier.height(300.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(5.dp),
+                    shape = CircleShape,
+                    border = BorderStroke(0.5.dp, Color.LightGray),
+                    elevation = 4.dp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.standard_image_150),
+                        contentDescription = "Profile image",
+                        modifier = Modifier.size(135.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
 
         }
     }
